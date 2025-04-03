c0c4d38864c79d56111cbcfb014cad31c4f3d057
RylVibra <168001434+RylVibra@users.noreply.github.com>
2025-04-03 19:19:00 +0000

    add qr adder func to push to worksheet

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,65 +2,34 @@ import streamlit as st
 import pandas as pd
 from streamlit_qrcode_scanner import qrcode_scanner
 from streamlit_gsheets import GSheetsConnection
+
 st.title("🎈 My QR app")
 
-
-# qrcode = qrcode_scanner()
-qrcode = "test"
-st.markdown(f"# {qrcode}")
-
-if 'df' not in st.session_state:
-    st.session_state.df = pd.DataFrame(columns=['QR codes'])
-
-
 def add_qr_code(code):
-    if code in st.session_state.df["QR codes"].values:
-        st.warning("This QR code already exists in the list.")
-        return
-    st.session_state.df.loc[len(st.session_state.df)] = [code]
-    st.session_state.last_qr = code
-
-def delete_last():
-    if "last_qr" in st.session_state:
-        # Remove all rows where "QR codes" match the last QR code
-        st.session_state.df = st.session_state.df[st.session_state.df["QR codes"] != st.session_state.last_qr]
-        del st.session_state.last_qr  # Clean up after deletion
-
-def delete_all():
-    st.session_state.df = pd.DataFrame(columns=["QR codes"])
-    st.session_state.pop("last_qr", None)  # Clear the last QR if it exists
-
-left, middle, right = st.columns(3, border=True, )
-
-with left:
-    if st.button("Add QR code"):
-        add_qr_code(qrcode)
-        st.success(f"Added: {qrcode}")
-
-with middle:
-    if st.button("Delete last"):
-        delete_last()
-
-with right:
-    if st.button("Delete all"):
-        delete_all()
-
-
-# st.dataframe(st.session_state.df, use_container_width=True)
-
-
-
+    df.loc[df["unit"] == selected_row, "battery"] = code
+    conn.update(data=df)
+    st.session_state.code = ""
 
 # Create a connection object.
 conn = st.connection("gsheets", type=GSheetsConnection)
+df = conn.read(ttl=0,usecols=[0, 1])
+all_units = df["unit"].unique().tolist()
+selected_row = st.sidebar.selectbox("Select a Unit", all_units)
 
-df = conn.read(
-    worksheet="Sheet2",
-    ttl="10m",
-    usecols=[0, 1],
-    nrows=3,
-)
+st.dataframe(
+    df.loc[df["unit"] == selected_row], 
+    use_container_width=True, 
+    hide_index=True
+    )
 
-# Print results.
-for row in df.itertuples():
-    st.write(f"{row.name} has a :{row.pet}:")
+
+if 'code' not in st.session_state:
+    st.session_state.code = ""
+
+qrcode = qrcode_scanner()
+# qrcode = "test"
+if qrcode:
+    st.session_state.code = qrcode
+st.markdown(f"{st.session_state.code}")
+    
+st.button("Add QR code", on_click=add_qr_code, args=(st.session_state.code,))
