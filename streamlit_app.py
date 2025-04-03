ef3b445b5d71be2f3095545c5faaba4b05c5efca
RylVibra <168001434+RylVibra@users.noreply.github.com>
2025-04-03 19:45:13 +0000

    remove session state for qr and add new QR detection toast

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,29 +7,31 @@ st.title("🎈 My QR app")
 
 def add_qr_code(code):
     df.loc[df["unit"] == selected_row, "battery"] = code
-    conn.update(data=df)
-    st.session_state.code = ""
-
+    if code in df["battery"].values:
+        st.warning("This QR code already exists in the database.")
+    else:
+        conn.update(data=df)
+        st.success("QR code added successfully.")
+    
 # Create a connection object.
 conn = st.connection("gsheets", type=GSheetsConnection)
 df = conn.read(ttl=0,usecols=[0, 1])
 all_units = df["unit"].unique().tolist()
-selected_row = st.sidebar.selectbox("Select a Unit", all_units)
+selected_row = st.selectbox("Select a Unit", all_units)
 
+# Display the selected row
 st.dataframe(
     df.loc[df["unit"] == selected_row], 
     use_container_width=True, 
     hide_index=True
     )
 
-
-if 'code' not in st.session_state:
-    st.session_state.code = ""
-
 qrcode = qrcode_scanner()
-# qrcode = "test"
-if qrcode:
-    st.session_state.code = qrcode
-st.markdown(f"{st.session_state.code}")
+last_qr = None
     
-st.button("Add QR code", on_click=add_qr_code, args=(st.session_state.code,))
+if qrcode!=last_qr:
+    st.toast("New code detected!", icon="📷")
+    last_qr = qrcode
+    
+st.markdown(f"{qrcode}")
+st.button("Add QR code", on_click=add_qr_code, args=(qrcode,))
