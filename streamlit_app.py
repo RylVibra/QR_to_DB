1c36d9d2f2c4f57b45ad14cf3688b684a9c85ab1
RylVibra <168001434+RylVibra@users.noreply.github.com>
2025-04-04 12:12:23 +0000

    fix DB write error and rename title

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,19 +3,19 @@ import pandas as pd
 from streamlit_qrcode_scanner import qrcode_scanner
 from streamlit_gsheets import GSheetsConnection
 
-st.title("🎈 My QR app")
+st.title("🎈 QR to Battery DB")
 
 def add_qr_code(code):
-    df.loc[df["unit"] == selected_row, "battery"] = code
     if code in df["battery"].values:
         st.warning("This QR code already exists in the database.")
     else:
+        df.loc[df["unit"] == selected_row, "battery"] = code
         conn.update(data=df)
         st.success("QR code added successfully.")
     
 # Create a connection object.
 conn = st.connection("gsheets", type=GSheetsConnection)
-df = conn.read(ttl=0,usecols=[0, 1])
+df = conn.read(ttl=0,usecols=[0, 1]).astype(str)
 all_units = df["unit"].unique().tolist()
 selected_row = st.selectbox("Select a Unit", all_units)
 
@@ -28,10 +28,11 @@ st.dataframe(
 
 qrcode = qrcode_scanner()
 last_qr = None
-    
-if qrcode!=last_qr:
-    st.toast("New code detected!", icon="📷")
-    last_qr = qrcode
+
+# if not last_qr and qrcode!=last_qr:
+    # st.toast("New code added!", icon="📷")
+    # last_qr = qrcode
     
 st.markdown(f"{qrcode}")
+# st.write(df.dtypes)
 st.button("Add QR code", on_click=add_qr_code, args=(qrcode,))
