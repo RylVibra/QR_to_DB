187aa9489b88478c5a434befd70c7d1342646508
RylVibra <168001434+RylVibra@users.noreply.github.com>
2025-04-04 13:32:22 +0000

    create a seperate scanner page

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,46 @@
 import streamlit as st
-import pandas as pd
-from streamlit_qrcode_scanner import qrcode_scanner
 from streamlit_gsheets import GSheetsConnection
 
-st.title("🎈 QR to Battery DB")
 
-def add_qr_code(code):
-    if code in df["battery"].values:
-        st.warning("This QR code already exists in the database.")
-    else:
-        df.loc[df["unit"] == selected_row, "battery"] = code
-        conn.update(data=df)
-        st.success("QR code added successfully.")
-    
+st.title("🔋 Battery Scanner")
+
 # Create a connection object.
 conn = st.connection("gsheets", type=GSheetsConnection)
 df = conn.read(ttl=0,usecols=[0, 1]).astype(str)
+
+# Create a selectbox for the user to select a unit
 all_units = df["unit"].unique().tolist()
 selected_row = st.selectbox("Select a Unit", all_units)
 
+
+if 'qrcode' not in st.session_state:
+    st.session_state.qrcode = ""
+
+qrcode = st.session_state.qrcode
+
+def add_qr_code(code):
+    df = conn.read(ttl=0,usecols=[0, 1]).astype(str)
+    df.loc[df["unit"] == selected_row, "battery"] = code
+    conn.update(data=df)
+    st.success("QR code added successfully.")
+    
+
 # Display the selected row
 st.dataframe(
-    df.loc[df["unit"] == selected_row], 
-    use_container_width=True, 
+    df.loc[df["unit"] == selected_row],
+    use_container_width=True,
     hide_index=True
     )
 
-qrcode = qrcode_scanner()
-last_qr = None
+st.page_link("1_scanner.py", label="Scan QR code", icon="📸", help="Scan QR code")
 
-# if not last_qr and qrcode!=last_qr:
-    # st.toast("New code added!", icon="📷")
-    # last_qr = qrcode
-    
+# pg = st.navigation([st.Page("page1.py", title="First page")])
+
+
+
+
+
+
+
 st.markdown(f"{qrcode}")
-# st.write(df.dtypes)
 st.button("Add QR code", on_click=add_qr_code, args=(qrcode,))
